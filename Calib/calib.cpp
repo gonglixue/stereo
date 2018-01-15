5fec603770404721e70b8f1a32e7115b7a5e64e3
gonglixue <724351282@qq.com>
2018-01-15 22:13:16 +0800

    calib offline

--- Calib/calib.cpp
+++ Calib/calib.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <stdio.h>
 /*
 comment reference
 http://blog.csdn.net/dcrmg/article/details/52929669
@@ -33,24 +34,20 @@ int main(int argc, char* argv[])
 	int board_n = board_w * board_h;
 	cv::Size board_sz = cv::Size(board_w, board_h);  // 多少个棋盘内角点
 
-	cv::VideoCapture capture(0);
-	if (!capture.isOpened()) {
-		cout << "couldn't open the camera\n";
-		help(argv);
-		return -1;
-	}
-
 	vector< vector<cv::Point2f> > image_points;
 	vector< vector<cv::Point3f> > object_points;
 
 	// 循环直至获取包含n_boards个棋盘角点的图像
-	double last_captured_timestamp = 0;
 	cv::Size image_size;
 
-	while (image_points.size() < (size_t)n_boards)
+    int input_count = 0;
+	while (input_count < n_boards)
 	{
 		cv::Mat image0, image;
-		capture >> image0;
+        char input_filename[20];
+        sprintf(input_filename, "../calib_example/Image%d.tif", (input_count + 1));
+		image0 = cv::imread(input_filename);
+
 		image_size = image0.size();
 		cv::resize(image0, image, cv::Size(), image_sf, image_sf, cv::INTER_LINEAR);  //变为原来大小的1/2
 
@@ -59,15 +56,11 @@ int main(int argc, char* argv[])
 		bool found = cv::findChessboardCorners(image, board_sz, corners);  // corners得到的是图像坐标吗？
 		cv::drawChessboardCorners(image, board_sz, corners, found);
 
-		// If we got a good board, add it to our data
-		double timestamp = (double)clock() / CLOCKS_PER_SEC;
-
-		if (found && timestamp - last_captured_timestamp > 1) {
-			last_captured_timestamp = timestamp;
+		if (found) {
 			image ^= cv::Scalar::all(255);	// ?
 
 			cv::Mat mcorners(corners);		// ?
-			mcorners *= (1. / image_sf);
+			mcorners *= (1. / image_sf);    // mcorners原图中的图像坐标
 			image_points.push_back(corners);
 			object_points.push_back(vector<cv::Point3f>());
 			vector<cv::Point3f>& opts = object_points.back();
@@ -76,13 +69,15 @@ int main(int argc, char* argv[])
 			{
 				opts[j] = cv::Point3f((float)(j / board_w), (float)(j%board_w), 0.0f);  // 世界坐标是：棋盘平面z=0, 棋盘左上角为原点？
 			}
-			cout << "Collected our " << (int)image_points.size() << " of" << 
+			cout << "Collected our " << (int)image_points.size() << " of " <<
 				n_boards << " needed chessboard images\n" << endl;
 
 		}
 		cv::imshow("calibration", image);
 
-		if ((cv::waitKey(30) & 255) == 27)
+        input_count++;
+
+		if ((cv::waitKey(120) & 255) == 27)
 			return -1;
 	}
 	// END COLLECTION WHILE LOOP
@@ -134,11 +129,15 @@ int main(int argc, char* argv[])
     );
 
     // show raw and undistorted image
-    for(;;){
+    for(int i=1; i<=20; i++){
         cv::Mat image, image0;
-        capture >> image0;
+        char filename[20];
+        sprintf(filename, "../calib_example/Image%d.tif", i);
+        image0 = cv::imread(filename);
+
         if(image0.empty())
             break;
+
         cv::remap(
                 image0,
                 image,
@@ -149,6 +148,10 @@ int main(int argc, char* argv[])
                 cv::Scalar()
         );
         cv::imshow("undistored", image);
+        char save_filename[20];
+        sprintf(save_filename, "../calib_example/undistored_Image%d.tif", i);
+        cv::imwrite(save_filename, image);
+
         if((cv::waitKey(30) & 255) == 27)
             break;
     }
