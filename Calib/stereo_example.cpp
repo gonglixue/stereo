dae1309679e59a0bbca8e64fd73142778388ae07
gonglixue <724351282@qq.com>
2018-01-18 22:01:00 +0800

    stereo example

--- Calib/stereo_example.cpp
+++ Calib/stereo_example.cpp
@@ -1,8 +1,11 @@
 //
 // Created by gonglixue on 18-1-17.
 //
+// ref
+// http://sourishghosh.com/2016/stereo-calibration-cpp-opencv/
 
 #include <opencv2/opencv.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -78,7 +81,7 @@ static void StereoCalib(const char* imageList, int nx, int ny, bool useUncalibra
                 resize(img, timg, cv::Size(), s, s, cv::INTER_CUBIC);
             found[lr] = cv::findCirclesGrid(
                     timg,
-                    cv::Size_(nx, ny),
+                    cv::Size(nx, ny),
                     corners[lr],  // image coordinates
                     cv::CALIB_CB_ASYMMETRIC_GRID | cv::CALIB_CB_CLUSTERING
             );
@@ -168,4 +171,151 @@ static void StereoCalib(const char* imageList, int nx, int ny, bool useUncalibra
     /*
      * =============================rectification=======================================
      */
+     if(showUndistorted){
+         cv::Mat R1, R2, P1, P2, map11, map12, map21, map22;
+         if(!useUncalibrated){
+             cv::stereoRectify(
+                     M1, D1, M2, D2,
+                     imageSize,
+                     R, T, R1, R2, P1, P2,
+                     cv::noArray(), 0
+             );
+             isVerticalStereo = fabs(P2.at<double>(1,3)) > fabs(P2.at<double>(0, 3));
+             // precompute maps for cvRemap()
+             cv::initUndistortRectifyMap(
+                     M1, D1, R1, P1, imageSize, CV_16SC2, map11, map12
+             );
+             cv::initUndistortRectifyMap(
+                     M2, D2, R2, P2, imageSize, CV_16SC2, map21, map22
+             );
+         }
+         else{
+            // use intrinsic parameters of each camera, bug compute the rectification transformation directly from the fundamental matrix
+             std::vector<cv::Point2f> allpoints[2];
+             for(i=0; i<nframes; i++){
+                 std::copy(
+                         points[0][i].begin(),
+                         points[0][i].end(),
+                         std::back_inserter(allpoints[0])
+                 );
+
+                 std::copy(
+                         points[1][i].begin(),
+                         points[1][i].end(),
+                         std::back_inserter(allpoints[1])
+                 );
+
+             }
+             cv::Mat F = cv::findFundamentalMat(
+                     allpoints[0], allpoints[1], cv::FM_8POINT
+             ); // 建立像素坐标之间的变换关系
+             cv::Mat H1, H2;
+             cv::stereoRectifyUncalibrated(
+                     allpoints[0], allpoints[1],
+                     F,
+                     imageSize,
+                     H1, H2,
+                     3
+             );
+
+             R1 = M1.inv() * H1 * M1;
+             R2 = M2.inv() * H2 * M2;
+
+             cv::initUndistortRectifyMap(
+                     M1, D1, R1, P1,
+                     imageSize,
+                     CV_16SC2,
+                     map21, map22
+             );
+
+         }
+         // rectify the images and find disparity maps
+         cv::Mat pair;
+         if(!isVerticalStereo)
+             pair.create(imageSize.height, imageSize.width*2, CV_8UC3);
+         else
+             pair.create(imageSize.height*2, imageSize.width, CV_8UC3);
+
+         // setup for finding stereo correspondences
+//         cv::Ptr<cv::StereoSGBM> stereo = cv::StereoSGBM::create(
+//                 -64, 128, 11, 100, 1000,
+//                 32, 0, 15, 1000, 16,
+//                 cv::StereoSGBM::MODE_HH
+//         );
+         cv::StereoSGBM stereo(
+                 -64, 128, 11, 100, 1000,
+                 32, 0, 15, 1000, 16,
+                 false
+         );
+
+
+
+
+         for(i=0; i<nframes; i++)
+         {
+             cv::Mat img1 = cv::imread(imageNames[0][i].c_str(), 0);
+             cv::Mat img2 = cv::imread(imageNames[1][i].c_str(), 0);
+             cv::Mat img1r, img2r, disp, vdisp;
+
+             if(img1.empty() || img2.empty())
+                 continue;
+
+             cv::remap(img1, img1r, map11, map12, cv::INTER_LINEAR);
+             cv::remap(img2, img2r, map21, map22, cv::INTER_LINEAR);
+
+             if(!isVerticalStereo || useUncalibrated){
+                 //stereo.compute(img1r, img2r, disp);
+                 stereo.operator()(img1r, img2r, disp);
+                 cv::normalize(disp, vdisp, 0, 256, cv::NORM_MINMAX, CV_8U);
+                 cv::imshow("disparity", vdisp);
+             }
+             if(!isVerticalStereo)
+             {
+                 cv::Mat part = pair.colRange(0, imageSize.width);
+                 cv::cvtColor(img1r, part, cv::COLOR_GRAY2BGR);
+                 part = pair.colRange(imageSize.width, imageSize.width*2);
+                 cv::cvtColor(img2r, part, cv::COLOR_GRAY2BGR);
+
+                 for(j=0; j<imageSize.height; j+=16)
+                     cv::line(
+                             pair,
+                             cv::Point(0, j),
+                             cv::Point(imageSize.width*2, j),
+                             cv::Scalar(0, 255, 0)
+                     );
+             }else{
+                 cv::Mat part = pair.rowRange(0, imageSize.height);
+                 cv::cvtColor(img1r, part, cv::COLOR_GRAY2BGR);
+                 part = pair.rowRange(imageSize.height, imageSize.height*2);
+                 cv::cvtColor(img2r, part, cv::COLOR_GRAY2BGR);
+
+                 for(j=0; j<imageSize.height; j+=16)
+                     cv::line(
+                             pair,
+                             cv::Point(j, 0),
+                             cv::Point(j, imageSize.height*2),
+                             cv::Scalar(0, 255, 0)
+                     );
+             }
+
+             cv::imshow("rectified", pair);
+             if((cv::waitKey()&255) == 27)
+                 break;
+         }
+
+     }
+}
+
+int main(int argc, char** argv)
+{
+    int board_w = 9, board_h = 6;
+    const char* board_list = "ch12_list.txt";
+    if(argc == 4){
+        board_list = argv[1];
+        board_w = atoi(argv[2]);
+        board_h = atoi(argv[3]);
+    }
+
+    StereoCalib(board_list, board_w, board_h, true);
+    return 0;
 }
