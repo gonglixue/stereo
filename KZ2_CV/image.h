cfd386586759b222e8c8589b8e0deb590e98fb6b
gonglixue <724351282@qq.com>
2018-02-02 16:58:19 +0800

    match(partial)

--- KZ2_CV/image.h
+++ KZ2_CV/image.h
@@ -2,7 +2,7 @@
 #define IMAGE_H
 
 #include <stdlib.h>
-#include <opencv2\opencv.hpp>
+#include <opencv2/opencv.hpp>
 
 typedef enum
 {
@@ -12,9 +12,9 @@ typedef enum
 	IMAGE_FLOAT
 }ImageType;
 
-inline cv::Vec3b imgRef(cv::Mat im, int x, int y)
+inline cv::Vec3b imgRef(cv::Mat im, int y, int x)
 {
-	cv::Vec3b value = im.at<cv::Vec3b>(x, y);
+	cv::Vec3b value = im.at<cv::Vec3b>(y, x);
 }
 
 inline int imGetXSize(cv::Mat im)
@@ -25,6 +25,63 @@ inline int imGetXSize(cv::Mat im)
 inline int imGetYSize(cv::Mat im)
 {
 	return im.rows;
+}
+
+struct Coord
+{
+	int x, y;
+	Coord() {}
+	Coord(int a, int b) { x = a; y = b; }
+
+	Coord operator+ (Coord a) const { return Coord(x + a.x, y + a.y); }
+	Coord operator+ (int a) const { return Coord(x + a, y); }
+	Coord operator- (int a) const { return Coord(x - a, y); }
+
+	bool operator< (Coord a) const { return (x < a.x) && (y < a.y); }
+	bool operator<= (Coord a) const { return (x <= a.x) && (y <= a.y); }
+	bool operator!= (Coord a) const { return (x != a.x) || (y != a.y); }
+};
+
+inline cv::Vec3b imgRef(cv::Mat im, Coord p)
+{
+	return imgRef(im, p.y, p.x);
+}
+
+// p是否在矩形r里
+inline bool inRect(Coord p, Coord r)
+{
+	return (Coord(0, 0) <= p && p < r);
+}
+
+// rectangle iterator
+class RectIterator {
+	Coord p;		///< current point
+	int w;			///< width of rectangle
+public:
+	RectIterator(Coord rect) :p(0, 0), w(rect.x) {}
+	const Coord& operator*() const { return p; };
+	bool operator!= (const RectIterator& it) const { return (p != it.p); }
+	RectIterator& operator++() {
+		if (++p.x == w) {
+			p.x = 0;
+			++p.y;
+		}
+		return *this;
+	}
+
+	friend RectIterator rectBegin(Coord rect);
+	friend RectIterator rectEnd(Coord rect);
+};
+
+inline RectIterator rectBegin(Coord rect) {
+	return RectIterator(rect);
+}
+
+inline RectIterator rectEnd(Coord rect)
+{
+	RectIterator it(rect);
+	it.p.y = rect.y;
+	return it;
 }
 
 
